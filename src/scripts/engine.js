deaeb80ad73d909f5eb79437dfbe6face7a545ae
Mallta <pggian1985@gmail.com>
2023-12-24 03:17:42 -0300

    add key verification rule

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,13 +1,26 @@
 const pianoKeys = document.querySelectorAll('.piano-keys .key');
-
+let mapedKeys =[];
 let audio = new Audio('src/tunes/a.wav');
 
 const playTune = (key) => {
-  audio.src =`src/tunes/${key}.wav`
-  audio.play()
+  audio.src = `src/tunes/${key}.wav`;
+  audio.play();
+
+  const clickedKey = document.querySelector(`[data-key="${key}"]`);
+  clickedKey.classList.add('active');
+  setTimeout(() => {
+    clickedKey.classList.remove('active');
+  }, 150);
 };
 
 pianoKeys.forEach((key) => {
   const keyData = key.dataset.key;
   key.addEventListener('click', () => playTune(keyData));
+  mapedKeys.push(keyData);
+});
+
+document.addEventListener('keydown', (event) => {
+  if(mapedKeys.includes(event.key)){
+    playTune(event.key);
+  }
 });
